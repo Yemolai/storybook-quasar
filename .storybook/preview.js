b2f95bc47dea6bfe0ac6fe7506c74e0a33f67a44
Gabriel R <yemolai@gmail.com>
2020-02-08 01:15:45 -0300

    importing quasar features into storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+// Setup context for Storybook here
+import 'quasar/dist/quasar.min.css'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+
+import 'quasar/dist/quasar.css'
+// import 'src/css/app.scss'
+
+import Vue from 'vue';
+import Quasar from 'quasar';
+
+Vue.use(Quasar, { config: {}, directives: {} });
+
+// run needed boot plugins files here
