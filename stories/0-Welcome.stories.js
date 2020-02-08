08026e6b150eef42cfbdec622392dce93f13fc4a
Gabriel R <yemolai@gmail.com>
2020-02-08 00:51:59 -0300

    formatting to tell eslint to stfu

--- stories/0-Welcome.stories.js
+++ stories/0-Welcome.stories.js
@@ -1,18 +1,18 @@
-import { linkTo } from '@storybook/addon-links';
+import { linkTo } from '@storybook/addon-links'
 
-import Welcome from './Welcome';
+import Welcome from './Welcome'
 
 export default {
   title: 'Welcome',
-  component: Welcome,
-};
+  component: Welcome
+}
 
 export const ToStorybook = () => ({
   components: { Welcome },
   template: '<welcome :showApp="action" />',
-  methods: { action: linkTo('Button') },
-});
+  methods: { action: linkTo('Button') }
+})
 
 ToStorybook.story = {
-  name: 'to Storybook',
-};
+  name: 'to Storybook'
+}
--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,29 +1,29 @@
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
 
-import MyButton from './MyButton';
+import MyButton from './MyButton'
 
 export default {
   title: 'Button',
-  component: MyButton,
-};
+  component: MyButton
+}
 
 export const Text = () => ({
   components: { MyButton },
   template: '<my-button @click="action">Hello Button</my-button>',
-  methods: { action: action('clicked') },
-});
+  methods: { action: action('clicked') }
+})
 
 export const Jsx = () => ({
   components: { MyButton },
-  render(h) {
-    return <my-button onClick={this.action}>With JSX</my-button>;
+  render (h) {
+    return <my-button onClick={this.action}>With JSX</my-button>
   },
-  methods: { action: linkTo('clicked') },
-});
+  methods: { action: linkTo('clicked') }
+})
 
 export const Emoji = () => ({
   components: { MyButton },
   template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-  methods: { action: action('clicked') },
-});
+  methods: { action: action('clicked') }
+})
